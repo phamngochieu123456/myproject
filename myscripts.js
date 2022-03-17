5dbb70aed4b63144bc9fff90013763157da3d0d1
phamngochieu123456 <pnhieu0128@gmail.com>
2022-03-17 15:06:43 +0700

    first commit

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+function submit_click()
+{
+    let email = document.getElementById("email").value;
+    let fullname = document.getElementById("fullname").value;
+    alert("OK")
+    console.log(email,fullname)
+}
