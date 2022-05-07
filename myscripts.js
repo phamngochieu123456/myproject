5694e25d7e2c9754c0630da69a8b8b00c609db9d
phamngochieu123456 <pnhieu0128@gmail.com>
2022-05-07 16:49:30 +0700

    make the project clean

--- myscripts.js
+++
@@ -1,7 +0,0 @@
-function submit_click()
-{
-    let email = document.getElementById("email").value;
-    let fullname = document.getElementById("fullname").value;
-    alert("OK")
-    console.log(email,fullname)
-}
